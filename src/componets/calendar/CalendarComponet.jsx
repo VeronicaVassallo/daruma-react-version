4c5624ff5e55412d30929803de03516bdc984735
VeronicaVassallo <vvassallo94@gmail.com>
2024-08-22 12:19:15 +0200

    fix calendar

---
+++ src/componets/calendar/CalendarComponet.jsx
@@ -0,0 +1,10 @@
+/*TO DO: crea un calendario, usando anche una libreria , 
+che permetta all'utente di aggiungere i compiti del giorno*/
+const CalendarComponet = () => {
+	return (
+		<div>
+			<p>Giorno: </p>
+			<h2>Lista cose da fare:</h2>
+		</div>
+	);
+};
